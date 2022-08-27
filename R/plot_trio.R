05e47e4f0cc42b06b97e86bf3185f2e87e6ead24
Yushi Tang <yushifelixtang@gmail.com>
2022-08-27 16:07:20 -0400

    PMCosm (Version 0.1.1)
    PMCosm (Version 0.1.1) update

--- R/plot_trio.R
+++ R/plot_trio.R
@@ -5,6 +5,7 @@
 #' @param param_trio  The n by 3 matrix of MLE parameter trio estimates.
 #' @param point_size  The numeric number between 0 and 1 for the point size. 
 #' @param a The numeric number between 0 and 1 for the alpha transparency level.
+#' @param zoom_in If TRUE, zoom in the majority of points. Default is FALSE.
 #'
 #' @return 
 #' Nothing of interest.
@@ -17,26 +18,51 @@
 #'
 #'
 #' @export
-plot_trio <- function(param_trio, point_size=0.8, a=0.6){
-  ggplot() +
-    geom_point(data=param_trio, 
-               aes(x=log(k), y=log(mu), color=pi0),alpha=a,size=point_size) +
-    ylab(TeX('\\log{(\\hat{\\mu})}')) +
-    xlab(TeX('\\log{(\\hat{k})}')) +
-    ggtitle(TeX('')) +
-    theme_bw() +
-    theme(aspect.ratio=1) +
-    scale_color_continuous(type = "viridis") +
-    labs(color=TeX('$\\hat{\\pi}_0$')) +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          axis.title.x = element_text(size = 12),
-          axis.title.y = element_text(size = 12),
-          axis.text.x = element_text(size = 9),
-          axis.text.y = element_text(size=9),
-          strip.text = element_text(size = 12),
-          legend.title = element_text(color = "black", size = 12),
-          legend.text = element_text(color = "black", size = 9))
+plot_trio <- function(param_trio, point_size=0.8, a=0.6, zoom_in=FALSE){
+  if(zoom_in){
+    ggplot() +
+      geom_point(data=param_trio, 
+                 aes(x=log(k), y=log(mu), color=pi0),alpha=a,size=point_size) +
+      ylab(TeX('\\log{(\\hat{\\mu})}')) +
+      xlab(TeX('\\log{(\\hat{k})}')) +
+      xlim(-5,20) +
+      ylim(-5,10) +
+      ggtitle(TeX('')) +
+      theme_bw() +
+      theme(aspect.ratio=1) +
+      scale_color_continuous(type = "viridis") +
+      labs(color=TeX('$\\hat{\\pi}_0$')) +
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            axis.title.x = element_text(size = 12),
+            axis.title.y = element_text(size = 12),
+            axis.text.x = element_text(size = 9),
+            axis.text.y = element_text(size=9),
+            strip.text = element_text(size = 12),
+            legend.title = element_text(color = "black", size = 12),
+            legend.text = element_text(color = "black", size = 9))
+  } else {
+    ggplot() +
+      geom_point(data=param_trio, 
+                 aes(x=log(k), y=log(mu), color=pi0),alpha=a,size=point_size) +
+      ylab(TeX('\\log{(\\hat{\\mu})}')) +
+      xlab(TeX('\\log{(\\hat{k})}')) +
+      ggtitle(TeX('')) +
+      theme_bw() +
+      theme(aspect.ratio=1) +
+      scale_color_continuous(type = "viridis") +
+      labs(color=TeX('$\\hat{\\pi}_0$')) +
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            axis.title.x = element_text(size = 12),
+            axis.title.y = element_text(size = 12),
+            axis.text.x = element_text(size = 9),
+            axis.text.y = element_text(size=9),
+            strip.text = element_text(size = 12),
+            legend.title = element_text(color = "black", size = 12),
+            legend.text = element_text(color = "black", size = 9))
+  }
+  
   
 }
 
